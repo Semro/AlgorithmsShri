6022d114769ab3474444e7f426aad0c4de264789
Semyon Rozhkov <semro@outlook.com>
2023-08-27 00:59:30 +0300

    feat: insertion-sort

--- problems/insertion-sort/insertion-sort.js
+++ problems/insertion-sort/insertion-sort.js
@@ -1,7 +1,21 @@
 // https://sortvisualizer.com/insertionsort/
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-export function insertionSort(arr) {
+function swap(arr, i, k) {
+  let temp = arr[i];
+  arr[i] = arr[k];
+  arr[k] = temp;
+}
 
-    return arr;
+export function insertionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let k = i;
+
+    while (arr[k] > arr[k + 1]) {
+      swap(arr, k, k + 1);
+      k--;
+    }
+  }
+
+  return arr;
 }
