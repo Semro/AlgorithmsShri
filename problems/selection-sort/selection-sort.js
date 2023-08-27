c6c95e933fab2ac844966f0e0a619fd5843a19d1
Semyon Rozhkov <semro@outlook.com>
2023-08-27 17:54:32 +0300

    feat: add selection-sort

--- problems/selection-sort/selection-sort.js
+++ problems/selection-sort/selection-sort.js
@@ -1,7 +1,23 @@
 // https://sortvisualizer.com/selectionsort/
 // https://en.wikipedia.org/wiki/Selection_sort
 
-export function selectionSort(arr) {
+function swap(arr, i, k) {
+  [arr[i], arr[k]] = [arr[k], arr[i]];
+}
 
-    return arr;
+export function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = arr[i];
+    let minIndex;
+    let k = i + 1;
+    for (; k < arr.length; k++) {
+      if (arr[k] < min) {
+        minIndex = k;
+        min = arr[k];
+      }
+    }
+    if (minIndex !== undefined) swap(arr, i, minIndex);
+  }
+
+  return arr;
 }
